00e3e3f63ddf3b57a4abf8641978805b547dd68f
Michael Kwaku Aboagye <35949773+Rev0kz@users.noreply.github.com>
2019-04-15 15:17:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,102 @@
-from flask import Flask, request, jsonify
-import json 
-import psycopg2  
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+app = Flask(__name__)  
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:lablam@localhost:5432/novelstore'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+
+db = SQLAlchemy(app)
+
+
+
+class Authors(db.Model):
+          
+          name = db.Column(db.String(), unique=True, primary_key=True)
+          book = db.Column(db.String(), unique=True)
+
+
+          def __init__(self, name, book): 
+		self.name = name
+		self.book = book 
+    
+          
+
+   	  def __repr__(self):   
+		return "<Name: {}>".format(self.name)   
+
+
+          def serialize(self):
+		return {
+            		'name':  self.name, 
+            		'book' : self.book
+           		}
+    
+    
+@app.route('/authors', methods=['GET'])
+def get_authors():
+       
+	if request.method == 'GET': 
+		all_authors = Authors.query.all() 
+		return jsonify([e.serialize() for e in all_authors])	
+
+
+
+@app.route('/authors', methods=['POST'])
+def create_author():
+	if request.method == 'POST':
+		name=request.args.get('name')
+		book=request.args.get('book')  
+		
+	try:
+        	author=Authors(
+            	   name=name,
+            	   book=book
+                 )
+        	db.session.add(author)
+        	db.session.commit()
+		return "Author added. author name={}".format(author.name) 
+	except Exception as e:
+		return(str(e))    
+
+
+@app.route('/authors/<name>', methods=['PUT'])
+def update():
+	if request.method == 'PUT':
+		author = Authors.query.get(name)
+		name = request.json['name'] 
+		book = request.json['book'] 
+		
+
+		author.name = name
+		author.book = book 
+
+                db.session.commit()
+		return jsonify([e.serialize() for e in author])
+
+
+@app.route('/authors/<name>', methods=['DELETE'])
+def del_author():
+    if request.method == 'DELETE':  
+		author = Authors.query.get(name)
+		db.session.delete(author)
+		db.session.commit() 
+		return "Author deleted. author name={}".format(author.name)
+
+
+       
+if __name__ == '__main__':  
+	app.run(debug=True, port=5000)
+
 
  
 
-@app.route('/api/v1/info', methods=['GET'])
-def get_info():
-     conn = psycopg2.connect(user='postgres',
-                             password='cantwinkatyou',
-                             host='127.0.0.1',
-                             port='5432',
-                             database='bookstore')
-     print("database connected successfully")
-     cursor = conn.cursor()
-     table_select_query = "select * from apiinfo"
-     cursor.execute(table_select_query)
-     print("retrieving values from the apiinfo table")
-     apiinfo_records = cursor.fetchall()
-     api_list=[] 
-     for row in apiinfo_records: 
-       a_dict = {}
-       a_dict['buildate'] = row[0]
-       a_dict['version'] = row[1]
-       a_dict['methods'] = row[2]
-       api_list.append(a_dict)
-     conn.close()
-     return jsonify({'api_version' : api_list}), 200      
+
+    
 
 
-
-@app.route('/api/v1/authors', methods=['GET', 'POST])
-def get_authors():   
-     conn = psycopg2.connect(user='postgres',
-                             password='cantwinkatyou',
-                             host='127.0.0.1',
-                             port='5432',
-                             database='bookstore')  
-    print("database connected successfully")
-    cursor = conn.cursor()
-    table_select_query = "select * from authors"
-    cursor.execute(table_select_query)
-    print("retrieving values from the authors table")
-    authors_records = cursor.fetchall() 
-    authors=[]  
-    for row in authors_records:  
-      a_dict = {}
-      a_dict['ID'] = row[0]
-      a_dict['name'] = row[1] 
-      a_dict['country'] = row[2]
-      a_dict['gender'] =row[3]    
-      a_dict['book'] = row[4]   
-      authors.append(a_dict)
-    conn.close()
-    return jsonify({'authors_list' : authors}), 200     
+   
 
 
 
